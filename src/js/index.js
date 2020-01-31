68730f58deee210168f566cec02813c4875cd328
Colin Gillespie <ratchetandcolin@gmail.com>
2020-01-30 22:04:05 -0500

    Added animation.css and AOS animations

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,11 @@
 import '../style/style.scss'
 import { elements } from "./views/base.js";
 
+import AOS from 'aos';
+// import '../style/aos.css';
+// ..
+
+
 // Events
 
 elements.sideNavBtn.addEventListener('click', () => {
@@ -15,4 +20,10 @@ elements.sideNavOption.forEach(option => {
     option.addEventListener('click', () => {
         elements.sideNav.style.visibility = 'hidden';
     });
+});
+
+AOS.init({
+    once: true,
+    duration: 1000,
+    delay: 200
 });
